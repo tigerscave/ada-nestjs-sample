cf01719c467486fd61c329d380429b1a14a2ae0c
cdra0624 <t.adaniya@adawarp.com>
2019-06-01 22:59:56 +0900

    fix(ormconfig): use .js for entity file

--- ormconfig.js
+++ ormconfig.js
@@ -6,6 +6,6 @@ module.exports = {
   "password": process.env.DB_PASSWORD,
   "database": process.env.DATABASE,
   "ssl": process.env.SSL === "TRUE",
-  "entities": ["src/**/*.entity{.ts,.js}"],
+  "entities": process.env.NODE_ENV === 'development' ? ["src/**/*.entity{.ts,.js}"] : "dist/**/*.entity.js",
   "synchronize": true
 }
